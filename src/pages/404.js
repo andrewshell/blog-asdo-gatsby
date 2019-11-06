a9927240b40a3c1e3849878695336005e46ce13a
Andrew Shell <andrew.shell@johnsonfit.com>
2019-11-06 10:07:15 -0600

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,19 +2,14 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import geekityLogo from "../images/geekity_full.svg";
+import Search from "../components/search";
 
 function NotFoundPage() {
   return (
     <Layout>
-      <SEO title="404: Not found" />
-      <div>
-        <img
-          alt="Ghost getting abducted by aliens"
-          className="block mx-auto w-1/2"
-          src={ geekityLogo }
-        />
-      </div>
+      <SEO title="404: Not Found" />
+      <h1>404 Page Not Found</h1>
+      <Search />
     </Layout>
   );
 }
