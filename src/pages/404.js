0b4b3263b946808a56880d5b6172df357729c294
Andrew Shell <andrew@andrewshell.org>
2021-10-27 13:29:28 -0500

    New 404 page style

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,13 @@ import SEO from "../components/seo";
 function NotFoundPage() {
   return (
     <Layout>
-      <SEO title="404: Not Found" />
-      <h1>404 Page Not Found</h1>
+      <main id="gh-main" class="gh-main">
+        <article class="gh-article post no-image">
+          <header class="gh-article-header gh-canvas">
+            <h1 class="gh-article-title">404 Page Not Found</h1>
+          </header>
+        </article>
+      </main>
     </Layout>
   );
 }
